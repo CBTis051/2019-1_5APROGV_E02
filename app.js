473d49b6e6ed9fd392fa0931fe3e534430fd610a
Ángel Alejandro Haya Ramírez <ahaya@cbtis051.edu.mx>
2019-11-19 16:54:26 -0700

    Commit inicial del server

---
+++ app.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+
+    res.end('Hola mundo Node JS Repo GIT - Haya Ramírez Ángel Alejandro\n');
+}).listen(8081);
+console.log('Server running on port 8081');
+
+  
