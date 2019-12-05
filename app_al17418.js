cc251de91c9d7c4466352739d8881ff4101da361
José Rodolfo Chávez González <al17440@cbtis051.edu.mx>
2019-12-04 23:00:18 -0700

    Se agrega package.json

--- app_al17418.js
+++ app_al17418.js
@@ -1,7 +1,14 @@
 var http = require('http');
-http.createServer(function (req,res) {
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+http.createServer(function (req, res) {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf8'});
 
-    res.end('Hola mundo NOde JS Repo GIT - Sanchez Vera Karla\n');
-}).listen(8081);
-console.log('Server running on port 8081');
+        var objeto = {
+            mensaje : 'Hola mundo Node JS Repo GIT - Sanchez Vera Karla\n'
+        };
+
+       var json = JSON.stringify(objeto);
+
+        res.end(json);
+
+    }).listen(8081);
+    console.log('Server running on port 8081');
