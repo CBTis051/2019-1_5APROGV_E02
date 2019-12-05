a9e3225e2393fa15adb781b22274d2df9816cdea
José Rodolfo Chávez González <al17440@cbtis051.edu.mx>
2019-12-05 00:57:58 -0700

    seguro muy seguro

---
+++ server.js
@@ -0,0 +1,26 @@
+require('./config/config');
+
+const express = require('express');
+const mysql = require('mysql2')
+
+const app = express();
+
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}))
+// parse application/json
+app.use(bodyParser.json())
+
+/**
+ * Home del server --> /
+ */
+app.get('/', (req, res) => {
+
+    let objeto = {
+        mensaje: "Bienvenidos al REST server - 5APROGV Equipo02"
+    };
+    res.send(objeto);
+});
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port ", process.env.PORT);
+});
